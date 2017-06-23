0f854d528bf6208d35a48e99cd3b397add2dc1e4
Nicolai Parlog <nipa@codefx.org>
2017-06-23 22:10:43 +0200

    Make Travis build the site

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -3,6 +3,11 @@
 # exit with nonzero exit code if anything fails
 set -e
 
+# SSH agent was shut down, so start it again, adding the deploy key
+eval `ssh-agent -s`
+ssh-add deploy-key
+
+# now commit and push
 cd $1
 git add --all
 git commit -m "Update site"
