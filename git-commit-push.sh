bdf6e5834d143fa859e9e63867e3cddf5b58cf7a
Nicolai Parlog <nipa@codefx.org>
2017-06-27 22:30:33 +0200

    Include commit hashes of sources and projects in commit message

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -3,12 +3,23 @@
 # exit with nonzero exit code if anything fails
 set -e
 
-# SSH agent was shut down, so start it again, adding the deploy key
-eval `ssh-agent -s`
-ssh-add deploy-key
+# on Travis, SSH agent was shut down, so start it again, adding the deploy key
+if [ "$1" == "launch-ssh" ]
+then
+	eval `ssh-agent -s`
+	ssh-add deploy-key
+fi
+
+# gather hashes for commit message
+cd site-source
+SOURCE_HASH="$(git rev-parse --short HEAD)"
+cd ../junit-pioneer
+XP_HASH="$(git rev-parse --short HEAD)"
+cd ..
 
 # now commit and push
-cd $1
+cd site
 git add --all
-git commit -m "Update site"
+# echo "Update to source $SOURCE_HASH, xp $XP_HASH"
+git commit -m "Update to source $SOURCE_HASH, xp $XP_HASH"
 git push
